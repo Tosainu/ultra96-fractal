262697217ed6a0cd5e3a5ce46633a6f291f9d0f6
Kenta Sato <tosainu.maple@gmail.com>
2019-01-14 18:42:30 +0900

    Change target clock frequency (150MHz -> 125MHz)

--- hls_ip/fractal/script.tcl
+++ hls_ip/fractal/script.tcl
@@ -6,7 +6,7 @@ add_files "$script_dir/fractal.cc" -cflags "-std=c++11"
 add_files -tb "$script_dir/fractal_tb.cc" -cflags "-std=c++11"
 open_solution "solution1"
 set_part {xczu3eg-sbva484-1-e} -tool vivado
-create_clock -period 6.67 -name default
+create_clock -period 8.00 -name default
 # csim_design -ldflags {-B/usr/lib/x86_64-linux-gnu/} -O
 csynth_design
 # cosim_design -ldflags {-B/usr/lib/x86_64-linux-gnu/}
