78a8206c4304a89a86ccde020a8a5a8ad32ca60a
Kenta Sato <tosainu.maple@gmail.com>
2019-02-09 14:57:20 +0900

    Update testbench

--- hls_ip/fractal/fractal_tb.cc
+++ hls_ip/fractal/fractal_tb.cc
@@ -8,18 +8,12 @@
 
 static constexpr auto OUTPUT_IMAGE = "out.ppm";
 
-cv::Mat fractal_cpu(std::uint32_t width, std::uint32_t height) {
+cv::Mat fractal_cpu(std::uint32_t width, std::uint32_t height, fix64_type x1, fix64_type y1,
+                    fix64_type dx, fix64_type dy, fix64_type offset_x, fix64_type offset_y,
+                    fix64_type cr, fix64_type ci) {
   cv::Mat dst(height, width, CV_8UC3);
 
-  const auto x1 = fix64_type{1.0};
-  const auto y1 = fix64_type{width} / fix64_type{height};
-  const auto dx = fix64_type{2.0} * x1 / fix64_type{MAX_WIDTH};
-  const auto dy = fix64_type{2.0} * y1 / fix64_type{MAX_HEIGHT};
-
-  const auto offset_x = fix64_type{0};
-  const auto offset_y = fix64_type{0};
-
-  const auto c = std::complex<fix64_type>{-0.4, 0.6};
+  const auto c = std::complex<fix64_type>{cr, ci};
 
   constexpr auto color_table = make_color_table();
 
@@ -50,8 +44,19 @@ cv::Mat fractal_cpu(std::uint32_t width, std::uint32_t height) {
 auto main() -> int {
   cv::Mat dst(MAX_HEIGHT, MAX_WIDTH, CV_8UC3);
 
+  const double ratio  = MAX_HEIGHT / MAX_WIDTH;
+  const double scale  = 1.0;
+  const auto x1       = fix64_type{1.0 / scale};
+  const auto y1       = fix64_type{ratio / scale};
+  const auto dx       = fix64_type{2 * x1 / MAX_WIDTH};
+  const auto dy       = fix64_type{2 * y1 / MAX_HEIGHT};
+  const auto offset_x = fix64_type{0.0};
+  const auto offset_y = fix64_type{0.0};
+  const auto cr       = fix64_type{-0.4};
+  const auto ci       = fix64_type{0.6};
+
   stream_type stream_out;
-  fractal(stream_out);
+  fractal(x1, y1, dx, dy, offset_x, offset_y, cr, ci, stream_out);
   AXIvideo2cvMat(stream_out, dst);
 
   // GBR2BGR
@@ -60,7 +65,8 @@ auto main() -> int {
   std::swap(channels[0], channels[1]);
   cv::merge(channels, dst);
 
-  const auto dst_cpu = fractal_cpu(MAX_WIDTH, MAX_HEIGHT);
+  const auto dst_cpu =
+      fractal_cpu(MAX_WIDTH, MAX_HEIGHT, x1, y1, dx, dy, offset_x, offset_y, cr, ci);
 
   const auto diff = cv::sum(dst - dst_cpu);
   if (diff[0] || diff[1] || diff[2]) {
